9f4088dbe0325ee62f572e70cbffc1afb3eba421
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-08 22:16:24 +0530

    updated Index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
@@ -7,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
+    <Toaster></Toaster>
     </BrowserRouter>
   
 );
