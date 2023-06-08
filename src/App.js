b166b82a87bc76c3141be93d86c747247d6351dc
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-08 23:35:21 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
   return( <div>
     <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></Navbar> 
     <Routes>
-        <Route path="/" element={<Home></Home>}></  Route>
+        <Route path="/" element={<Home></Home>}></Route>
   
-        <Route path="/login" element={<Login></Login>}  ></Route>
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}></Login>}  ></Route>
   
-        <Route path="/signup" element={<Signup></ Signup>}></Route>
+        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}></Signup>}></Route>
         
-        <Route path="/dashboard" element={<Dashboard></ Dashboard>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
 
     </Routes>
   </div>
