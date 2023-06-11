624e040a43fd8b926ceaadc21238342cd6f43bc3
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-11 07:26:21 +0530

    updated dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 function Dashboard() {
   return (
-    <div className="flex flex-1 justify-center items-center text-3xl">
+    <div className="flex h-full justify-center items-center text-3xl text-white">
       Welcome to Studynotion!
     </div>
   );
